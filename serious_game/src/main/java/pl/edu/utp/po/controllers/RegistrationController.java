4dfb139e63cc297839073e002cd7eb1079d86d46
Jonasz <jonkul001@gmail.com>
2018-11-23 03:15:56 +0200

    registration redirect attributes

--- serious_game/src/main/java/pl/edu/utp/po/controllers/RegistrationController.java
+++ serious_game/src/main/java/pl/edu/utp/po/controllers/RegistrationController.java
@@ -26,34 +26,34 @@ public class RegistrationController {
     @PostMapping("/register")
     public String addToUsers(Users user, String login, String email, String pass, String pass2,
                              Model model, RedirectAttributes redirectAttributes) {
-        boolean valid[] = {false,false,false,false,false,false};
+        boolean valid[] = {false, false, false, false, false, false};
         String error = "";
 
         if (registerService.findByLogin(login).isEmpty()) { //if there's no this login in database
             valid[0] = true;
-            if(login_validation(login)){
+            if (login_validation(login)) {
                 valid[1] = true;
             } else error += " Wrong User Name. At least 3 signs. Without special signs.\n";
         } else error += " This User Name is not available.\n";
 
 
-        if (registerService.findByEmail(email).isEmpty()){
+        if (registerService.findByEmail(email).isEmpty()) {
             valid[2] = true;
-            if(email_validation(email)){
-                valid[3]=true;
+            if (email_validation(email)) {
+                valid[3] = true;
             } else error += " Wrong email.\n";
         } else error += " This email is not available.\n";
 
 
-        if (login_validation(pass)){
+        if (login_validation(pass)) {
             valid[4] = true;
-            if(pass2.equals(pass)){
-                valid[5]=true;
+            if (pass2.equals(pass)) {
+                valid[5] = true;
             } else error += " Password confirmation error.\n";
         } else error += " Wrong password. At least 3 signs. Without special signs.\n";
 
-        for(boolean b:valid){
-            if(!b){
+        for (boolean b : valid) {
+            if (!b) {
                 model.addAttribute("alert1", error);
                 return "/register";
             }
@@ -63,12 +63,12 @@ public class RegistrationController {
         return "redirect:/login";
     }
 
-    private boolean login_validation(String login){
+    private boolean login_validation(String login) {
         Matcher matcher = Pattern.compile("^[a-zA-Z0-9._-]{3,}$", Pattern.CASE_INSENSITIVE).matcher(login);
         return matcher.find();
     }
 
-    private boolean email_validation(String email){
+    private boolean email_validation(String email) {
         Matcher matcher = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE).matcher(email);
         return matcher.find();
     }
