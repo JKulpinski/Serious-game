3740399add5ea3446bcb3dbab6695a0317480ee4
Jonasz <jonkul001@gmail.com>
2018-12-04 17:37:40 +0200

    Count points in rebus

--- serious_game/src/main/resources/static/css/js/rebus.js
+++ serious_game/src/main/resources/static/css/js/rebus.js
@@ -2,11 +2,15 @@ var game = {
     words: new Array(),
     img: 1111,
     run: true,
+    points: 0,
     a_word: function (w) {
         this.words.push(w);
     },
     t_word: function () {
         return this.words.pop();
+    },
+    update_points: function () {
+        document.getElementById("info").innerHTML = 'Current points: ' + this.points;
     }
 };
 
@@ -28,30 +32,32 @@ document.addEventListener('keydown', function (event) {
         guess_word = guess_word.split(' ').join('');
         if (guess_word === game.words[game.words.length - 1].toUpperCase()) {
             alert("Good answer :)");
-            wordlist.push(game.t_word());
-            if (game.words.length === 0) {
-                game.run = false;
-                document.getElementById("pojemnik").innerHTML = '<br>Finish!<br>' +
-                    '<span style="font-size:x-large;text-align:left">' +
-                    'Found answers:<ul id = "list">' +
-                    '</ul></span>';
-                var mylist = document.getElementById("list");
+            game.points += 1;
+            game.update_points();
+        }
+        else {
+            alert("Bad answer!!!");
+        }
+        wordlist.push(game.t_word());
+        if (game.words.length === 0) {
+            game.run = false;
+            document.getElementById("container").innerHTML = '<br>Finish!<br>' +
+                '<span style="font-size:x-large;text-align:left">' +
+                'Found answers:<ul id = "list">' +
+                '</ul></span>';
+            var mylist = document.getElementById("list");
 
-                for (var i = 0; i < wordlist.length; i++) {
-                    var newlistitem = document.createElement('li');
-                    newlistitem.textContent = wordlist[i];
-                    mylist.appendChild(newlistitem);
-                }
-            }
-            else {
-                game.img++;
-                var pic = "images/" + game.img + ".jpg";
-                document.getElementById("picture").innerHTML = '<img style="width: 50% " src="' + pic + '".jpg" alt =""/>';
-                document.getElementById("error").innerHTML ='';
+            for (var i = 0; i < wordlist.length; i++) {
+                var newlistitem = document.createElement('li');
+                newlistitem.textContent = wordlist[i];
+                mylist.appendChild(newlistitem);
             }
         }
         else {
-            document.getElementById("error").innerHTML ='Bad answer!!! Try again.';
+            game.img++;
+            var pic = "images/" + game.img + ".jpg";
+            document.getElementById("picture").innerHTML = '<img style="width: 50% " src="' + pic + '".jpg" alt =""/>';
+            document.getElementById("error").innerHTML = '';
         }
         if (game.run === true) document.getElementById('odp').value = "";
     }
