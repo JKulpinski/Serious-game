75be38eb3505adccff51681aaf4fec107af4717e
nalia3486 <nalia3486@gmail.com>
2018-12-14 17:30:51 +0200

    new table

--- baza_prog_ob_spring.sql
+++ baza_prog_ob_spring.sql
@@ -213,3 +213,81 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 14 Gru 2018, 16:30
+-- Wersja serwera: 10.1.36-MariaDB
+-- Wersja PHP: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `baza_prog_ob_spring`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `picture`
+--
+
+CREATE TABLE `picture` (
+  `id` bigint(20) NOT NULL,
+  `answer` varchar(255) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `level` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `picture`
+--
+
+INSERT INTO `picture` (`id`, `answer`, `text`, `level`) VALUES
+                                                               (1, 'one', '1', 1),
+                                                               (2, 'two', '2', 1),
+                                                               (3, 'three', '3', 1),
+                                                               (4, 'four', '4', 1),
+                                                               (5, 'five', '5', 1),
+                                                               (6, 'six', '6', 1),
+                                                               (7, 'seven', '7', 1),
+                                                               (8, 'eight', '8', 1),
+                                                               (9, 'nine', '9', 1),
+                                                               (10, 'ten', '10', 1);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `picture`
+--
+ALTER TABLE `picture`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `picture`
+--
+ALTER TABLE `picture`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
