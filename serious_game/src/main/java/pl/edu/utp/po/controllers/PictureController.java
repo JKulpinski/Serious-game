b71c9f5788e2532cf1e5a460560e348cb68c9167
Jonasz <jonkul001@gmail.com>
2018-12-14 21:21:39 +0200

    adding pictures points to database and journey buttons changes

--- serious_game/src/main/java/pl/edu/utp/po/controllers/PictureController.java
+++ serious_game/src/main/java/pl/edu/utp/po/controllers/PictureController.java
@@ -6,10 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.edu.utp.po.domain.Picture;
 import pl.edu.utp.po.domain.Users;
-import pl.edu.utp.po.repositories.PictureRepository;
 import pl.edu.utp.po.services.PictureService;
 import pl.edu.utp.po.services.RegisterService;
 
@@ -25,7 +23,6 @@ public class PictureController {
 
     @Autowired
     private RegisterService registerService;
-
 
     @GetMapping("/pictures")
     public String showPictures(Model model, HttpServletRequest req) {
@@ -52,17 +49,15 @@ public class PictureController {
     }
 
     @PostMapping("/pictures")
-    public String addPoints(HttpServletRequest req, String point, RedirectAttributes redirectAttributes) {
+    public String addPoints(HttpServletRequest req, String point) {
         HttpSession session = req.getSession();
         Users user = (Users) session.getAttribute("user");
-        if (user.getRebus()) { //cheatowanie
-            redirectAttributes.addFlashAttribute("second_try", "You've done this game on this level before!");
+        if (user.getPicture()) { //cheatowanie
             return "redirect:/journey";
         }
         user.setPoints(user.getPoints() + Integer.valueOf(point));
-        user.setRebus(!user.getRebus()); // zmienione na nieaktywny rebus po wygranej na danym levelu
+        user.setPicture(!user.getPicture()); // zmienione na nieaktywny rebus po wygranej na danym levelu
         registerService.addUser(user);
         return "redirect:/journey";
     }
-
 }
--- serious_game/src/main/java/pl/edu/utp/po/repositories/PictureRepository.java
+++ serious_game/src/main/java/pl/edu/utp/po/repositories/PictureRepository.java
@@ -1,7 +1,6 @@
 package pl.edu.utp.po.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pl.edu.utp.po.domain.Hangman;
 import pl.edu.utp.po.domain.Picture;
 
 import java.util.List;
