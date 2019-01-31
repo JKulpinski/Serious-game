a33ac2c804103e3cd85e545da95148485b34c801
nalia3486 <nalia3486@gmail.com>
2019-01-31 18:25:07 +0200

    war file

--- serious_game/src/main/java/pl/edu/utp/po/Lab3JpaApplication.java
+++ serious_game/src/main/java/pl/edu/utp/po/Lab3JpaApplication.java
@@ -1,11 +1,12 @@
 package pl.edu.utp.po;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Lab3JpaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Lab3JpaApplication.class, args);
 
--- serious_game/src/test/java/com/manning/readinglist/ReadinglistApplicationTests.java
+++ serious_game/src/test/java/com/manning/readinglist/ReadinglistApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import pl.edu.utp.po.Lab3JpaApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = Lab3JpaApplication.class)
 public class ReadinglistApplicationTests {
 
 	@Test
