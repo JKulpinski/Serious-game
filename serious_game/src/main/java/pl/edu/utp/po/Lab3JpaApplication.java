4e8eaf6724bf8146ac208c12943f489c5862d2c1
Jonasz <jonkul001@gmail.com>
2019-01-31 19:24:44 +0200

    Last changes

--- serious_game/src/main/java/pl/edu/utp/po/Lab3JpaApplication.java
+++ serious_game/src/main/java/pl/edu/utp/po/Lab3JpaApplication.java
@@ -1,14 +1,21 @@
 package pl.edu.utp.po;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Lab3JpaApplication {
+public class Lab3JpaApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Lab3JpaApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Lab3JpaApplication.class, args);
 
     }
+
 }
